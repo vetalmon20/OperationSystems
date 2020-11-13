e4177c41b20ee8db82dcfddaf5b5b5a510023397
Vitaliy Datsiuk <vetalvetal222@gmail.com>
2020-11-13 11:00:53 +0200

    Implemented BakeryLock

--- Lab2/src/main/java/com/university/Main.java
+++ Lab2/src/main/java/com/university/Main.java
@@ -1,15 +1,12 @@
 package com.university;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Long> threadsToEnter = new HashSet<>(2);
-        System.out.println(threadsToEnter.size());
-        threadsToEnter.add(1L);
-        threadsToEnter.add(2L);
-        threadsToEnter.remove(1L);
-        System.out.println(threadsToEnter.size());
+
     }
 }
